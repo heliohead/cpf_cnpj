3e3f8bdbf1dc06d690299eb097ad05f8e6bc6527
Helio Oliveira <hheelliioo@live.com>
2021-11-19 21:07:50 -0300

    Sunrise 🌅

---
+++ lib/cpf_cnpj.ex
@@ -0,0 +1,18 @@
+defmodule CpfCnpj do
+  @moduledoc """
+  Documentation for `CpfCnpj`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CpfCnpj.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cpf_cnpj_test.exs
@@ -0,0 +1,8 @@
+defmodule CpfCnpjTest do
+  use ExUnit.Case
+  doctest CpfCnpj
+
+  test "greets the world" do
+    assert CpfCnpj.hello() == :world
+  end
+end
