c776dfa48aa9b8cb5c6e44ce094c83df81dd23d2
Helio Oliveira <hheelliioo@live.com>
2021-11-20 19:26:24 -0300

    Add valid? fun

--- lib/cpf.ex
+++ lib/cpf.ex
@@ -15,4 +15,46 @@ defmodule Cpf do
   def strip(cpf) do
     String.replace(cpf, ~r/[\.\/-]/, "")
   end
+
+  @doc """
+  Verify if cpf is valid
+
+  ## Examples
+
+      iex> Cpf.valid?("073.055.920-30")
+      true
+
+  """
+  def valid?(cpf) do
+    digits_verifier?(cpf)
+  end
+
+  defp digits_verifier?(cpf) do
+    list = cpf |> to_list()
+    cpf_digits = list |> Enum.take(-2)
+    without_verifier = list |> Enum.take(9)
+    first_verifier = without_verifier |> gen_verifier()
+    second_verifier = (without_verifier ++ [first_verifier]) |> gen_verifier()
+
+    cpf_digits == [first_verifier, second_verifier]
+  end
+
+  defp to_list(cpf) do
+    cpf |> strip() |> String.split("", trim: true) |> Enum.map(&String.to_integer/1)
+  end
+
+  defp gen_verifier(numbers) do
+    modulus = Enum.count(numbers) + 1
+
+    mod =
+      numbers
+      |> Enum.with_index()
+      |> Enum.map(fn {number, index} ->
+        number * (modulus - index)
+      end)
+      |> Enum.sum()
+      |> rem(11)
+
+    if mod < 2, do: 0, else: 11 - mod
+  end
 end
--- test/cpf_test.exs
+++ test/cpf_test.exs
@@ -1,8 +1,15 @@
-defmodule CPFTest do
+defmodule CpfTest do
   use ExUnit.Case
   doctest Cpf
 
   test "strip" do
     assert Cpf.strip("415.276.500-30") == "41527650030"
+  end
+
+  test "valid?" do
+    assert Cpf.valid?("415.276.500-30")
+    assert Cpf.valid?("68894480062")
+    refute Cpf.valid?("415.276.500-00")
+    refute Cpf.valid?("41527650000")
   end
 end
